20a360f39615a960eecf7d8ca5d97c3bd6515e61
1005574346 <60927301+1005574346@users.noreply.github.com>
2020-06-09 17:19:02 -0500

    Add files via upload

---
+++ js/validar.js
@@ -0,0 +1,7 @@
+function validar{
+    var nombre,correo,telefono,expresion;
+   nombre =document.getElementById("nombre").nodeValue
+   correo =document.getElementById("correo").nodeValue
+   telefono =document.getElementById("telefono").nodeValue
+
+}
